afce7458c5ac95b98ff9e24c3f940cf74b6cea34
Hydra <ryanandrews135@gmail.com>
2020-05-18 10:08:16 -0600

    Add settings descriptions

--- vUI_PluginTemplate/vUI_PluginTemplate.lua
+++ vUI_PluginTemplate/vUI_PluginTemplate.lua
@@ -7,11 +7,18 @@ local vUI, GUI, Language, Assets, Settings, Defaults = vUIGlobal:get()
 -- Create a plugin
 local Plugin = vUI:NewPlugin("vUI_PluginTemplate")
 
--- Create default setting values
-Defaults["some-value"] = true
-Defaults["some-other-value"] = 12
+-- Set default values if creating new settings
+Defaults["some-value"] = true -- After profiles are loaded, this is accessed with Settings["some-value"]
+Defaults["other-value"] = 12
 
 -- Operations to run when the plugin loads
 function Plugin:Load()
 	
 end
+
+-- Create a settings window for the plugin. Use self:GetWindow("Name") to access an existing window
+GUI:AddOptions(function(self)
+	local Left, Right = self:CreateWindow("Template")
+	
+	-- Create window widgets
+end)
