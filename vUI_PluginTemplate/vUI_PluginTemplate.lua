4280ccdcf57f31d29fd78a20a3f2fa5f330a35f4
Hydra <ryanandrews135@gmail.com>
2020-12-15 10:56:57 -0700

    Updates for the GUI changes

--- vUI_PluginTemplate/vUI_PluginTemplate.lua
+++ vUI_PluginTemplate/vUI_PluginTemplate.lua
@@ -2,23 +2,21 @@ if (not vUIGlobal) then
 	return
 end
 
-local vUI, GUI, Language, Media, Settings, Defaults = vUIGlobal:get()
+local vUI, GUI, Language, Assets, Settings, Defaults = vUIGlobal:get()
 
--- Create a plugin object
+-- Create a plugin object.
 local Plugin = vUI:NewPlugin("vUI_PluginTemplate") -- This name must match the plugin folder
 
--- Set default values if creating new settings
+-- Set default values if creating new settings.
 Defaults["some-value"] = true -- After profiles are loaded, this is accessed with Settings["some-value"]
 Defaults["other-value"] = 12
 
--- Operations to run when the plugin loads
+-- Operations to run when the plugin loads, if your plugin doesn't require this operation it can simply be removed.
 function Plugin:Load()
 	
 end
 
--- Create a settings window for the plugin. Use self:GetWindow("Name") to access an existing window
-GUI:AddOptions(function(self)
-	local Left, Right = self:CreateWindow("Template")
-	
+-- Create a settings window for the plugin. Change WindowName to your own desired window, or use the name of an existing window to add widgets.
+GUI:AddSettings(Language["General"], Language["WindowName"], function(left, right)
 	-- Create window widgets
 end)
