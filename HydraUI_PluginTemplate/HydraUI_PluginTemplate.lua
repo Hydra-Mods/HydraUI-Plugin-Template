6047faa807cef9d5982a8c4826ef66d82add6425
Hydra <ryanandrews135@gmail.com>
2021-07-12 23:21:38 -0600

    Correct the comments

--- HydraUI_PluginTemplate/HydraUI_PluginTemplate.lua
+++ HydraUI_PluginTemplate/HydraUI_PluginTemplate.lua
@@ -11,12 +11,12 @@ local Plugin = HydraUI:NewPlugin("HydraUI_PluginTemplate") -- This name must mat
 Defaults["some-value"] = true -- After profiles are loaded, this is accessed with Settings["some-value"]
 Defaults["other-value"] = 12
 
--- Operations to run when the plugin loads, if your plugin doesn't require this operation it can simply be removed.
-function Plugin:Load()
+-- Operations to run when the plugin loads, if your plugin doesn't require this function, it can simply be removed.
+function Plugin:Load() -- This function fires on PLAYER_ENTERING_WORLD after settings are initialized and all HydraUI modules have been loaded
 	
 end
 
 -- Create a settings window for the plugin. Change WindowName to your own desired window, or use the name of an existing window to add widgets.
-GUI:AddSettings(Language["General"], Language["WindowName"], function(left, right)
+GUI:AddSettings("General", "WindowName", function(left, right)
 	-- Create window widgets
 end)
